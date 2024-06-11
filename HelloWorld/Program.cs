96d72d1440952ac2997c6ad4758651bf160c6bd5
manlikeclaro <ckibii08@gmail.com>
2024-06-11 18:04:31 +0300

    [chore] initial commit c# fundamentals

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MyProgram
+{
+    class MyProgram
+    {
+        static void Main()
+        {
+            // double myNum = 15.55453426472674;
+            
+            Console.WriteLine("Enter num to display: ");
+            int myNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You entered: {myNum}");
+        }   
+    }
+}
