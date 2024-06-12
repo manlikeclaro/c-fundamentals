109d21171c7ccfef4e63f39f47574129a697ebf3
manlikeclaro <ckibii08@gmail.com>
2024-06-12 15:43:17 +0300

    [chore] learned basic oop

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,7 +2,33 @@
 {
     class MyProgram
     {
-        static void Main()
+        private string model;
+        private string colour;
+        private int year;
+
+        static int AddNums(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double AddDoubleNums(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void CarMapper(object obj)
+        {
+            Type type = obj.GetType();
+            var properties = type.GetProperties();
+            
+            foreach (var item in properties)
+            {
+                Console.WriteLine($"{item.Name}");
+                // Console.WriteLine($"{item.GetValue(obj)}");
+            }
+        }
+
+        static void Main(string[] args)
         {
             // double myNum = 15.55453426472674;
 
@@ -15,29 +41,34 @@
             // Console.WriteLine(myNumbers.Min());  // returns the smallest value
             // Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
 
-            // static void MyMethod(string child1, string child2 , string child3 = "darryl")
-            // {
-            //     Console.WriteLine("The youngest child is: " + child3);
-            //     Console.WriteLine(child3);
-            // }
 
-            static int AddNums(int x, int y)
-            {
-                return x + y;
-            }
+            // int addIntResult = AddNums(3, 5);
+            // Console.WriteLine($"add int result: {addIntResult}");
+            // double addDoubleResult = AddDoubleNums(3.0, 5.0);
+            // Console.WriteLine($"add int result: {addDoubleResult}");
+
+            // MyProgram program = new MyProgram();
+            // Console.WriteLine(program.colour);
+
+            MyProgram car1 = new MyProgram();
+            car1.colour = "red";
+            car1.model = "ferrari";
+            car1.year = 2019;
+
+            MyProgram car2 = new MyProgram();
+            car2.colour = "yellow";
+            car2.model = "porsche";
+            car2.year = 2011;
             
-            static double AddDoubleNums(double x, double y)
-            {
-                return x + y;
-            }
-
-            static void Main(string[] args)
-            {
-                int addIntResult = AddNums(3, 5);
-                Console.WriteLine($"add int result: {addIntResult}");
-                double addDoubleResult = AddDoubleNums(3.0, 5.0);
-                Console.WriteLine($"add int result: {addDoubleResult}");
-            }
+            // CarMapper(car1)?;
+            Console.WriteLine(car1.colour);
+            Console.WriteLine(car1.model);
+            Console.WriteLine(car1.year);
+            
+            Console.WriteLine(car2.colour);
+            Console.WriteLine(car2.model);
+            Console.WriteLine(car2.year);
+            
         }
     }
 }
